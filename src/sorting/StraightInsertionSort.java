fd30be4673365a497c46a2dd57639946a2fdf566
MorningRain2016 <2727728599@qq.com>
2019-07-21 22:09:51 +0800

    fix bug

--- src/sorting/StraightInsertionSort.java
+++ src/sorting/StraightInsertionSort.java
@@ -1,29 +1,36 @@
 package sorting;
 
+import java.util.Arrays;
+
+/**
+ * ÷±Ω”≤Â»Î≈≈–Ú
+ * @author 90698
+ *
+ */
 public class StraightInsertionSort {
 	static void insertSort(int arr[]){
 		if(arr == null || arr.length == 0 || arr.length == 1){
 			return;
 		}
 		
-		for(int i = 2; i < arr.length; i ++){
+		for(int i = 1; i < arr.length; i ++){
 			int currValue = arr[i];
-			
-			for(int j = i -1; j > 0; j--){
+			int j = i -1;
+			for(; j >= 0; j--){
 				if(currValue < arr[j]){
 					arr[j + 1] = arr[j];
 				} else {
-					arr[j] = currValue;
 					break;
 				}
 			}
-			
+			arr[j + 1] = currValue;
+			System.out.println(Arrays.toString(arr));
 		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] originArr = {7, 6, 5, 4, 3, 2, 1};
+		insertSort(originArr);
 	}
 
 }
